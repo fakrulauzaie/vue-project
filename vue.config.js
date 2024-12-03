1d55dd030fc04e9dcf54e71e86ad511b24aaba72
fakrulauzaie <fakrulauzaie@gmail.com>
2024-12-03 15:48:17 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
 module.exports = {
     publicPath: '/vue-project/'
 }
+
+export default defineConfig({
+    plugins: [
+      vue(),
+  
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    define: {
+      'process.env': {}
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    base: './',
+  
+  })
